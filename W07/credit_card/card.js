bcf43bc3f2c8e297da8db6d733f2a60b391c2f58
samuelpickett <pic21007@byui.edu>
2025-06-04 19:44:20 -0600

    Worked on Card Form

---
+++ W07/credit_card/card.js
@@ -0,0 +1,37 @@
+function isCardNumberValid(number) {
+	// normally we would contact a credit card service...but we don't know how to do that yet. So to keep things simple we will only accept one number
+	return number === '1234123412341234';
+}
+function displayError(msg) {
+	// display error message
+	document.querySelector('.errorMsg').innerHTML = msg;
+}
+function submitHandler(event) {
+	event.preventDefault();
+	let errorMsg = '';
+	console.log(this.cardNumber.value)
+	// clear any previous errors
+	displayError('');
+	// check credit card number
+	if (isNaN(this.cardNumber.value)) {
+		// it is not a valid number
+		errorMsg += 'Card number is not a valid number\n';
+	} else if (!isCardNumberValid(this.cardNumber.value)) {
+		// it is a number, but is it valid?
+		errorMsg += 'Card number is not a valid card number\n';
+	}
+    let input_date = new Date(this.year.value, this.month.value);
+    console.log(input_date)
+    
+
+	if (errorMsg !== '') {
+		// there was an error. stop the form and display the errors.
+		displayError(errorMsg);
+		return false;
+	}
+	return true;
+}
+
+
+
+document.getElementById("submit").addEventListener("submit", submitHandler);
